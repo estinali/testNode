68c6eed5950a3f330d2e5b42deab06b32c8fe2a8
O.Zh <estinali@gmail.com>
2024-01-20 22:34:13 +0300

    load planets data in test

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -4,11 +4,13 @@ const {
     mongoConnect,
     mongoDissconnect 
 } = require('../../services/mongo');
+const { loadPlanetsData } = require('../../models/planets.model');
 
 describe('Launches API', () => {
 
     beforeAll(async () => {
         await mongoConnect();
+        await loadPlanetsData();
     });
 
     afterAll(async () => {
